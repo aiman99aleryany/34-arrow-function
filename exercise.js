adf88721aea1e22bece1143d7b4febace6ebfb1a
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 21:40:23 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,12 @@
+// Replace the function below with arrow function.
+
+// function sum() {
+// return 5 + 5;
+// }
+
+// console.log(sum());
+// The output of the function doesn't have to change.
+
+let sum = (num1, num2) => num1 + num2;
+
+console.log(sum());
