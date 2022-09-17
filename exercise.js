b8018bc8f2e09518231dd2ca5be3d9a9f24c5d61
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 21:47:08 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,11 +1,4 @@
-// Replace the function below with arrow function.
-
-// function sum() {
-// return 5 + 5;
-// }
-
-// console.log(sum());
-// The output of the function doesn't have to change.
+// Starting from the exercise 33, create an arrow function that takes in two numbers as parameters and returns the sum.
 
 let sum = (num1, num2) => num1 + num2;
 
